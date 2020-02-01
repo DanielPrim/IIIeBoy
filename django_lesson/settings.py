a9a5039f3d556fd14f9271552d34b1091e80471a
Stan25 <redoffprim@gmail.com>
2020-02-01 12:20:20 +0300

    kiany rivs

--- django_lesson/settings.py
+++ django_lesson/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'django_lesson.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
